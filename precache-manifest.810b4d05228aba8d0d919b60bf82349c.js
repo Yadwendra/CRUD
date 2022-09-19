da6e1e4a64b763ca145fd96797d65247e994a6c4
Yadwendra <yadwendraec.hzp@gmail.com>
2022-09-19 21:45:25 +0530

    Updates

---
+++ precache-manifest.810b4d05228aba8d0d919b60bf82349c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2371bb81f9e58477445c",
+    "url": "/CRUD/static/css/main.aa190abb.chunk.css"
+  },
+  {
+    "revision": "2371bb81f9e58477445c",
+    "url": "/CRUD/static/js/main.2371bb81.chunk.js"
+  },
+  {
+    "revision": "e3ae2a948ccc016e30a3",
+    "url": "/CRUD/static/js/1.e3ae2a94.chunk.js"
+  },
+  {
+    "revision": "1f2666053f3b17455b53",
+    "url": "/CRUD/static/js/runtime~main.1f266605.js"
+  },
+  {
+    "revision": "cb0c026db849ac40eeec80b33fec2e3f",
+    "url": "/CRUD/index.html"
+  }
+];
